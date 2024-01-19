0e0e49daba187265b83420f04c513d5f8b6c4489
Henry Corredor González <henry.corredor7570@gmail.com>
2024-01-18 19:25:41 -0500

    ruta deleted

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000/api";
+const URL = import.meta.env.VITE_BACKEND_URL;
 
 const instance = axios.create({
     // baseURL: "http://localhost:3000/api",
@@ -9,3 +9,5 @@ const instance = axios.create({
 })
 
 export default instance;
+
+// || "http://localhost:3000/api"
