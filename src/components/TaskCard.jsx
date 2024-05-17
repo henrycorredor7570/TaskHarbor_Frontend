a06f43fcd1b5735939ae6893ea81431c769023dd
Henry Corredor González <78055527+henrycorredor7570@users.noreply.github.com>
2024-05-17 07:58:53 -0500

    component TaskCard

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -9,7 +9,7 @@ function TaskCard({task}) {
   return (
     <div className="bg-zinc-800 max-w-md w-full p-10 rounded-md">
         <header className="flex justify-between">
-            <h1 className="text-2xl font-bold">{task.title}</h1>
+            <h1 className="text-2xl font-bold">{task.title}</h1> 
             <div className="flex gap-x-2 items-center">
                 <button className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-md"
                     onClick={() => {
